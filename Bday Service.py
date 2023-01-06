311e52a775fbec449f901e24c750cc1b52668c60
AryanNYadav <104957787+AryanNYadav@users.noreply.github.com>
2023-01-06 10:17:35 +0530

    Update Bday Service.py

--- Bday Service.py
+++ Bday Service.py
@@ -1,150 +1,64 @@
-from datetime import date
-import Medium as M
 import mysql.connector
-import os
-
-# It is version2
-# Because I've created database on cloud.
-
-today = date.today()  # To get Today's Date.
-db = mysql.connector.connect(host="sql6.freesqldatabase.com", user="sql6582218", passwd="KNAZUQReNL",
-                             database="sql6582218")
-cursor = db.cursor()
-print('........................................Birthday Service and Festival..............................')
-
-
-def showData():
-    cursor.execute('SELECT * FROM Data')
-    data = cursor.fetchall()
-    for d in data:
-        print(d)
-    # cursor.execute('SELECT * FROM Festivals')
-    # data = cursor.fetchall()
-    # for d in data:
-    # print(d)
-
-
-def getData():
-    global Email_ID
-    global Month
-    global Name
-    global Number
-    global Holiday
-    global Date
-
-    Holiday, Date = [], []
-    Email_ID, Month, Name, Number = [], [], [], []
-    # for getting Birthday's table data/info
-    cursor.execute('SELECT * FROM Data')
-    data = cursor.fetchall()
-    for d in data:
-        Email_ID += [d[1]]
-        Month += [(str(d[3])).split('-')]
-        Name += [d[0]]
-        Number += [d[2]]
-        # print(d)
-
-    # For getting Festival's table data
-    cursor.execute('SELECT * FROM Festivals')
-    data = cursor.fetchall()
-    for d in data:
-        Holiday += [d[0]]
-        Date += [(str(d[1])).split('-')]
-
-
-def checkBirthdayToday():
-    getData()
-    for i in range(len(Month)):
-        for j in range(len(Month[i])):
-            if Month[i][j] == str(today.month) and Month[i][j - 1] == str(today.day):
-                print("Listout the name of Person's who have birthday today!")
-                print(Name[i])
-                M.sendMail(Name[i], Email_ID[i])  # It will send Mail to the person
-                M.sendWTPM(Name[i][j], Number[i])  # It will send Whatsapp Message to the person
-
-
-def checkFestivalToday():
-    getData()
-    for i in range(len(Date)):
-        for j in range(len(Date[i])):
-            if str(today.month) == Date[i][j] and str(today.day) == Date[i][j - 1]:
-                for filename in os.listdir('.'):
-                    if filename.startswith(Holiday[i]):
-                        Text = open(Holiday[i] + ".txt")
-                        Text = Text.read()  # Read or stored content of Holiday[i] toText variable.
-                        Photo = open(Holiday[i] + ".png")
-                        # This for loop will send emails to all present in table of Data.
-                        for mail in Email_ID:
-                            M.sendFM(Text, Photo.name, mail)
-
-                        # This for loop will send Whatsapp Message to all present in table of Data.
-                        for Number in Number:
-                            M.sendFMWT(Number, Photo.name, Text)
-                            Photo.close()  # Closed opened file
-
-
-checkFestivalToday()  # Festival Function Called
-checkBirthdayToday()  # Birthday Function Called
-
-from email.message import EmailMessage  # This will provides creating or modifying structured messages.
-import imghdr  # imghdr module-determines the type of image
-import os
-from PIL import Image  # (PIL)-Python Image Library
-import pywhatkit
+import datetime
+from twilio.rest import Client
 import smtplib
-import urllib.request  # request module defines functions and classes which helpinopening URLs
 
-msg = EmailMessage()
-Sender_Mail = "xyz516332@gmail.com"
-password = "ktzvzolvtabzdoya"
-
-
-def sendMail(Name, Reciver_Mail):
-    server = smtplib.SMTP('smtp.gmail.com', 587)  # Created gmail's server, and connected to gmail API
-    # Adding transfer layered security
-    server.starttls()
-    server.login(Sender_Mail, password)  # Email, App password are inserted.
-    msg['Subject'] = 'Testing of HAPPY BIRTHDAY Service!'
-    msg['From'] = Sender_Mail
-    msg['To'] = Reciver_Mail
-    msg.set_content(f'Happiest Birthday {Name}!\nI Enjoy a Lot......!')
-    urllib.request.urlretrieve(
-        'https://thumbs.dreamstime.com/b/colorful-happy- birthday-cupcakes-candles-spelling-148323072.jpg', "HBD.jpg")
-    with open('HBD.jpg', 'rb') as f:
-        file_data = f.read()
-        file_type = imghdr.what(f.name)
-        file_name = f.name
-    msg.add_attachment(file_data, maintype='image', subtype=file_type, filename=file_name)
-    server.send_message(msg)
-    server.quit()
+def todaysBirthdays():
+    cursor.execute("SELECT * FROM USER_DATA")
+    todaysBirthdays = cursor.fetchall()
+    for i in todaysBirthdays:
+        dob=i[-1]
+        if dob.month==today.month and dob.day==today.day:
+            continue
+        else:
+            todaysBirthdays.remove(i)
+    return todaysBirthdays
 
 
-def sendWTPM(Name, Number):
-    img = 'B1.jpg'
-    body = f'Many Many Returns of the Day {Name}!\nI hope all your birthday wishes and dreams come true.'
-    pywhatkit.sendwhats_image(Number, img, body, 15, False, 3)
-    # FM: Festival Message
+def sendBirthdayMail(birthdays):
+    print(birthdays)
+    for i in birthdays:
+        name=i[0]
+        eMail=i[1]
+        print(name,eMail)
+        server.sendmail(senderEmail, eMail, "Happy Birthday " + name)
+    print("Birthday mails sent to ",len(birthdays),"users")
 
 
-def sendFM(Text, Photo, Reciver_Mail):
-    server = smtplib.SMTP('smtp.gmail.com', 587)  # Created gmail's server, and connected to gmail API
-    # Adding transfer layered security
-    server.starttls()
-    server.login(Sender_Mail, password)  # Email, App password are inserted.
-    msg['Subject'] = 'Testing of Festival Service!'
-    msg['From'] = Sender_Mail
-    msg['To'] = Reciver_Mail
-    msg.set_content(f'{Text}')
-    with open(Photo, 'rb') as f:
-        file_data = f.read()
-        file_type = imghdr.what(f.name)
-        file_name = f.name
-        msg.add_attachment(file_data, maintype='image', subtype=file_type, filename=file_name)
-
-    server.send_message(msg)
-    server.quit()
+# def sendBirthdayMsg(birthdays):
+#     accSID = "<Twilio Accound SID>"
+#     authToken = "<twilio AUTH token>"
+#     twilioCli = Client(accSID, authToken)
+#     myTwilioNumber = '+15672863118'
+#     for i in birthdays:
+#         name=i[0]
+#         phone=i[2]
+#         message=twilioCli.messages.create(body="Happy Birthday"+name,from_=myTwilioNumber,to=phone)
+#     print("Birthday mails sent to "len(birthdays),"users")
 
 
-def sendFMWT(Number, Photo, Text):
-    body = f'{Text}'
-    pywhatkit.sendwhats_image(Number, Photo, body, 15, False, 3)
+if __name__=="__main__":
+    user="root"
+    pwd="iamamy"
+    db="se"
+    host="localhost"
+    today=datetime.date.today()
+    try:
+        mycon=mysql.connector.connect(user=user,password=pwd,database=db,host=host)
+        cursor=mycon.cursor()
+    except:
+        print("Error: Unable to connect to the database")
+        exit()
+    try:
+        server = smtplib.SMTP('smtp.gmail.com', 587)  # connecting to SMTP server at port 587
+        server.ehlo()
+        server.starttls()
+        senderEmail = 'aryannyadav2002@gmail.com'
+        senderPass = 'gdpmznhjhqijiuas'
+        server.login(senderEmail, senderPass)
+    except:
+        print("Unable to connect to the SMTP server")
+        exit()
+
+    todaysBirthdays = todaysBirthdays()
+    sendBirthdayMail(todaysBirthdays)
